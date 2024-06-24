5659f44c1b5ba9fd2a89ad5f5b4b160031f24b6c
ShivaChandu <nedurishivachandueee@gmail.com>
2024-06-24 12:14:16 +0530

    first commit

---
+++ springboot_email_demo/src/test/java/com/email/app/SpringbootEmailDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEmailDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
